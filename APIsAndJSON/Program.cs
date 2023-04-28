efc4ee410a110057d5ae4d856fd2583b69dd5572
Nina <boldurfaith@gmail.com>
2023-04-27 18:43:33 -0700

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool cont = true;
+
+            do
+            {
+                RonVSKanyeAPI.KanyeQuote();
+                RonVSKanyeAPI.RonQuote();
+
+                Console.WriteLine("Continue? Y or N?");
+                var response = Console.ReadLine().ToLower();
+
+                cont = (response == "n") ? false : true;
+
+            } while (cont);
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public RonVSKanyeAPI()
+        {
+        }
+
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            //kanye API Url
+            var kanyeURL = "https://api.kanye.rest";
+
+            //JSON repsonse stored in a variable
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            //Parses object
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"\"{kanyeQuote}\"\n");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            //Ron Swanson API URL
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            //JSON repsonse stored in a variable
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            //Parses object
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine(ronQuote);
+        }
     }
 }
